90f8ca847db360781818854a757738361aa0c57c
Josh Tompkin <tompkinjo@gmail.com>
2023-09-12 14:38:05 -0400

    Update file handling

--- src/RNAseq/gene_ontology/go_filter.py
+++ src/RNAseq/gene_ontology/go_filter.py
@@ -20,20 +20,17 @@ def filter_terms(
         in_file: TextIO,
         delimiter: str = ',',
         term_column: int = 1,
-        header: bool = True,
         to_filter: str = '',
         filter_path: str | None = None,
 ) -> list[str]:
     """Filter gene ontology terms from input file"""
     if filter_path:
         with open(filter_path, 'r', encoding='UTF-8') as filter_file:
-            to_filter = filter_file.readlines()
+            to_filter = [i.rstrip() for i in filter_file.readlines()]
     else:
-        to_filter = to_filter.split(';')
+        to_filter = [i.lstrip() for i in to_filter.split(';')]
     with in_file:
         in_reader = csv.reader(in_file, delimiter=delimiter)
-        if header:
-            next(in_reader)
         return [row for row in in_reader if row[term_column] not in to_filter]
 
 
@@ -42,21 +39,25 @@ def write_terms(
         terms: list[str],
         delimiter: str = '\t',
         format_out: bool = True,
+        header: bool = False,
         id_column: int = 2,
         pval_column: int = 4
 ) -> None:
     """Write gene ontology terms. Optionally format output for Revigo"""
     with out_file:
-        out_writer = csv.writer(out_file, delimiter=delimiter)
+        out_writer = csv.writer(out_file, delimiter=delimiter, quoting=csv.QUOTE_ALL)
         if format_out:
             out_writer.writerows(
-                [[row[id_column], row[pval_column]] for row in terms]
+                [[row[id_column], row[pval_column]] for row in terms[1:]]
             )
-        else:
-            out_writer.writerows(terms)
+            return None
+        if header:
+            out_writer.writerow(terms[0])
+        out_writer.writerows(terms[1:])
+        return None
 
 
-def main():
+def main(args: argparse.Namespace | None = None):
     parser = argparse.ArgumentParser(prog='gofilter',
                                      description='Filter gProfiler output and format for revigo')
 
@@ -72,8 +73,8 @@ def main():
                                help="Delimiter character for input. Defaults to `,'.")
 
     output_options = parser.add_argument_group('output options')
-    output_options.add_argument('-o', '--out', dest='out_file', nargs='?',
-                                type=argparse.FileType, default=sys.stdout,
+    output_options.add_argument('-o', '--out', dest='out_file',
+                                type=argparse.FileType('w', encoding='UTF-8'), default=sys.stdout,
                                 help="Path to output file. Writes to standard out if `-'. "+
                                 "Defaults to standard out.")
     output_options.add_argument('-p', '--pval-column', dest='pval_column', type=int, default=4,
@@ -82,11 +83,14 @@ def main():
     output_options.add_argument('-i', '--id-column', dest='id_column', type=int, default=2,
                                 help='Integer index of column containing gene ontology ids. '+
                                 'Index starts at 0. Defaults to 2. Only used if formatting output.')
+    output_options.add_argument('-s', '--out-delimieter', dest='out_delimiter', default='\t',
+                                help='Delimiter character for output. Defaults to tab.')
     output_options.add_argument('--no-format', dest='format_out', action='store_false',
                                 help='Do not format output for Revigo. Output will be '+
                                 'formatted like gProfiler.')
-    output_options.add_argument('-s', '--out-delimieter', dest='out_delimiter', default='\t',
-                                help='Delimiter character for output. Defaults to tab.')
+    output_options.add_argument('--header', dest='write_header', action='store_true',
+                                help='Write header of input file to output. Only writes if '+
+                                '--no-format is specified.')
 
     filter_options = parser.add_argument_group('filter options')
     filter_options.add_argument('-f', '--filter', dest='filter_terms',
@@ -101,13 +105,13 @@ def main():
 
     filtered_terms = filter_terms(args.gProfiler_file,
                                   delimiter=args.in_delimiter,
-                                  header=True,
                                   to_filter=args.filter_terms,
                                   filter_path=args.filter_path)
-    write_terms(args.out_path,
+    write_terms(args.out_file,
                 terms=filtered_terms,
                 delimiter=args.out_delimiter,
                 format_out=args.format_out,
+                header=args.write_header,
                 id_column=args.id_column,
                 pval_column=args.pval_column)
 
